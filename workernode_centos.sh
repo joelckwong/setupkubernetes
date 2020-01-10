c917b3727043df86013e4eb82c5b98b7bb32efe7
Joel Kwong <31670777+joelckwong@users.noreply.github.com>
2020-01-09 22:08:58 -0500

    Update workernode_centos.sh

--- workernode_centos.sh
+++ workernode_centos.sh
@@ -27,4 +27,4 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
-sudo perl -pi -e 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo perl -pi -e 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
